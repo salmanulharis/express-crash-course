c62a662d0e36232716de7bb1d92c807250db7b10
salmanul.haris <salmanul.haris@acodez.co.in>
2024-12-31 14:02:29 +0530

    Basic route using express

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
  const express = require('express');
+ const path = require('path');
 
  const app = express();
 
  app.get('/', (req, res) => {
     res.send('Hello World');
+ })
+
+ app.get('/about', (req, res) => {
+    res.send('About');
  })
 
  app.listen(8000, () => console.log(`Server is running on port 8000`));
