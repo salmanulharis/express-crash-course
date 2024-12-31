c0faaa798d3f4284fac030156a2e3f2ccc116d51
salmanul.haris <salmanul.haris@acodez.co.in>
2024-12-31 15:18:21 +0530

    Using Es module and using seperate file for routes

---
+++ routes/posts.js
@@ -0,0 +1,33 @@
+import express from 'express';
+const router = express.Router();
+
+let posts = [
+    {id: 1, title: "Post One"},
+    {id: 2, title: "Post Two"},
+    {id: 3, title: "Post Three"}
+];
+
+// Get all post
+router.get('/', (req, res) => {
+    const limit = parseInt(req.query.limit);
+
+    if(!isNaN(limit) && limit > 0){
+        res.status(200).json(posts.slice(0, limit));
+    } else {
+        res.status(200).json(posts);
+    }
+})
+
+// Get single post
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    
+    if(!post) {
+        res.status(404).json({msg: `A post with the id of ${id} was not found`});
+    } else {
+        res.status(200).json(post);
+    }
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
- const express = require('express');
- const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
  const port = process.env.PORT || 8000;
 
  const app = express();
@@ -7,33 +8,8 @@
  // setup static folder
 //  app.use(express.static(path.join(__dirname, 'public'))); 
 
-let posts = [
-    {id: 1, title: "Post One"},
-    {id: 2, title: "Post Two"},
-    {id: 3, title: "Post Three"}
-];
+// Routes
+app.use('/api/posts', posts);
 
-// Get all post
-app.get('/api/posts', (req, res) => {
-    const limit = parseInt(req.query.limit);
-
-    if(!isNaN(limit) && limit > 0){
-        res.status(200).json(posts.slice(0, limit));
-    } else {
-        res.status(200).json(posts);
-    }
-})
-
-// Get single post
-app.get('/api/posts/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-    
-    if(!post) {
-        res.status(404).json({msg: `A post with the id of ${id} was not found`});
-    } else {
-        res.status(200).json(post);
-    }
-});
 
  app.listen(port, () => console.log(`Server is running on port ${port}`));
