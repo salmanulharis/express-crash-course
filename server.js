7533eb2ff614bf4f1475f665ec30f30de752e6ad
salmanul.haris <salmanul.haris@acodez.co.in>
2024-12-31 18:54:18 +0530

    __dirname workaround

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
  const port = process.env.PORT || 8000;
 
+ // Get the directory name
+ const __filename = fileURLToPath(import.meta.url);
+ const __dirname = path.dirname(__filename);
+
  const app = express();
 
  // setup static folder
-//  app.use(express.static(path.join(__dirname, 'public'))); 
+ app.use(express.static(path.join(__dirname, 'public'))); 
 
 // Body parser middleware
 app.use(express.json());
