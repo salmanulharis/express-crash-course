0183b169743b39e5c9c80b922575fef8b4884cda
salmanul.haris <salmanul.haris@acodez.co.in>
2024-12-31 18:31:24 +0530

    Color package

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import path from 'path';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
  const port = process.env.PORT || 8000;
 
  const app = express();
@@ -19,6 +20,9 @@ app.use(logger);
 
 // Routes
 app.use('/api/posts', posts);
+
+// catch all error handler
+app.use(notFound);
 
 // Error handler
 app.use(errorHandler);
