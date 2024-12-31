a6e83c53ce7ab1ec8a45e90e527694a1b5a43210
salmanul.haris <salmanul.haris@acodez.co.in>
2024-12-31 14:23:00 +0530

    sending json data;

--- server.js
+++ server.js
@@ -4,6 +4,17 @@
  const app = express();
 
  // setup static folder
- app.use(express.static(path.join(__dirname, 'public')));
+//  app.use(express.static(path.join(__dirname, 'public'))); 
+
+let posts = [
+    {id: 1, title: "Post One"},
+    {id: 2, title: "Post Two"},
+    {id: 3, title: "Post Three"}
+];
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts);
+
+})
 
  app.listen(8000, () => console.log(`Server is running on port 8000`));
