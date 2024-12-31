442e29a6466f0b5e1876224e11d6f8a912bf533a
salmanul.haris <salmanul.haris@acodez.co.in>
2024-12-31 14:49:57 +0530

    Pasing params via route and getting it

--- server.js
+++ server.js
@@ -13,8 +13,15 @@ let posts = [
     {id: 3, title: "Post Three"}
 ];
 
+// Get all post
 app.get('/api/posts', (req, res) => {
     res.json(posts);
 })
+
+// Get single post
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    res.json(posts.filter((post) => post.id === id));
+});
 
  app.listen(port, () => console.log(`Server is running on port ${port}`));
