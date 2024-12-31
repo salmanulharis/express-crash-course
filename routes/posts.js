706b985305bd7fdfe6e3c4c1782183e602f83bc4
salmanul.haris <salmanul.haris@acodez.co.in>
2024-12-31 17:23:53 +0530

    Put Request

--- routes/posts.js
+++ routes/posts.js
@@ -44,4 +44,17 @@ router.post('/', (req, res) => {
     res.status(201).json(posts);
 })
 
+// Update Post
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    
+    if(!post) {
+        res.status(404).json({msg: `A post with the id of ${id} was not found`});
+    }
+
+    post.title = req.body.title;
+    res.status(200).json(posts);
+});
+
 export default router;
